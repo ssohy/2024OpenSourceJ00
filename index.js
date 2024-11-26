782c9a1bc26150b2158830ea4d48b377853059fe
ssohy <109409651+ssohy@users.noreply.github.com>
2024-11-27 01:22:20 +0900

    Update index.js

--- index.js
+++ index.js
@@ -216,6 +216,7 @@ app.post('/logout', (req, res) => {
     res.redirect('/signin');
 });
 
+
 // 서버 시작
 const PORT = process.env.PORT || 5006;
 app.listen(PORT, () => console.log(`Server running on  http://localhost:${PORT}`));
