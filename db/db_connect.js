7bc78b11e84545a11609322d59641a6a62d84da5
ssohy <sohyeonj415@gmail.com>
2024-12-06 13:58:08 +0000

    fix: db_connect.js 원상복구

--- db/db_connect.js
+++ db/db_connect.js
@@ -8,24 +8,9 @@ const dbInfo = {
     password: process.env.dbPassword,
     port: process.env.dbPort,
     database: process.env.dbName
-};
-
-// DB 연결 풀 생성
-const client = mysql.createPool(dbInfo);
-
-// DB 연결 테스트
-async function testConnection() {
-    try {
-        const connection = await client.getConnection();
-        console.log("DB 연결 성공:", dbInfo.host);
-        connection.release();  // 연결을 반환하여 풀에서 재사용되도록 합니다.
-    } catch (error) {
-        console.error("DB 연결 실패:", error.message);
-    }
 }
 
-// 연결 테스트 실행
-testConnection();
+const client = mysql.createPool(dbInfo)
 
 module.exports = {
     client
